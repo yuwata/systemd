1ba954a9f6255ad4f87434e1220f60e8e6c67c96
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-09 14:00:55 +0900

    include: define __THROW for musl
    __THROW is internally used by glibc headers, hence our implementation of
    net/if.h and sys/mount.h also use it. However, musl does not provide
    the macro. Let's define it when not defined.
    
    Upstream-Status: Inappropriate [musl specific]
    Signed-off-by: Yu Watanabe <watanabe.yu+github@gmail.com>

--- src/basic/include/features.h
+++ src/basic/include/features.h
@@ -6,3 +6,8 @@
 /* Most glibc headers includes features.h.
  * Let's define assert_cc() here, to make it usable in our glibc header wrappers. */
 #define assert_cc(expr) _Static_assert(expr, #expr)
+
+/* For musl. */
+#ifndef __THROW
+#define __THROW
+#endif
