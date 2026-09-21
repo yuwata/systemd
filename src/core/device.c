5368e6e6e5abf4010f9530d6e4613ad1c1b7a79e
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-09-21 15:55:38 +0900

    core/device: drop trivial 'main' argument for device_setup_unit()
    No functional change, just refactoring.

--- src/core/device.c
+++ src/core/device.c
@@ -660,7 +660,7 @@ static void device_upgrade_mount_deps(Unit *u) {
         }
 }
 
-static int device_setup_unit(Manager *m, sd_device *dev, const char *path, bool main, Set **units) {
+static int device_setup_unit(Manager *m, sd_device *dev, const char *path, Set **units) {
         _cleanup_(unit_freep) Unit *new_unit = NULL;
         _cleanup_free_ char *e = NULL;
         const char *sysfs = NULL;
@@ -726,7 +726,7 @@ static int device_setup_unit(Manager *m, sd_device *dev, const char *path, bool 
                         return log_unit_error_errno(u, r, "Failed to set sysfs path %s: %m", sysfs);
 
                 /* The additional systemd udev properties we only interpret for the main object */
-                if (main)
+                if (path_equal(sysfs, path))
                         (void) device_add_udev_wants(u, dev);
         }
 
@@ -800,7 +800,7 @@ static int device_setup_devlink_unit_one(Manager *m, const char *devlink, Set **
 
                 /* Note, even if the device is being processed by udevd, setup the unit on enumerate.
                  * See also the comments in device_catchup(). */
-                return device_setup_unit(m, dev, devlink, /* main= */ false, ready_units);
+                return device_setup_unit(m, dev, devlink, ready_units);
         }
 
         /* the devlink is already removed or not ready */
@@ -866,7 +866,7 @@ static int device_setup_extra_units(Manager *m, sd_device *dev, Set **ready_unit
                  * exist. To achieve that, they set the path to SYSTEMD_ALIAS. Hence, we cannot refuse
                  * aliases that start with /dev/, unfortunately. */
 
-                (void) device_setup_unit(m, dev, *alias, /* main= */ false, ready_units);
+                (void) device_setup_unit(m, dev, *alias, ready_units);
         }
 
         l = hashmap_get(m->devices_by_sysfs, syspath);
@@ -925,13 +925,13 @@ static int device_setup_units(Manager *m, sd_device *dev, Set **ret_ready_units,
                 /* Add the main unit named after the syspath. If this one fails, don't bother with the rest,
                  * as this one shall be the main device unit the others just follow. (Compare with how
                  * device_following() is implemented, see below, which looks for the sysfs device.) */
-                r = device_setup_unit(m, dev, syspath, /* main= */ true, &ready_units);
+                r = device_setup_unit(m, dev, syspath, &ready_units);
                 if (r < 0)
                         return r;
 
                 /* Add an additional unit for the device node */
                 if (sd_device_get_devname(dev, &devname) >= 0)
-                        (void) device_setup_unit(m, dev, devname, /* main= */ false, &ready_units);
+                        (void) device_setup_unit(m, dev, devname, &ready_units);
 
         } else {
                 Unit *u;
@@ -1272,7 +1272,7 @@ void device_found_node(Manager *m, const char *node, DeviceFound found, DeviceFo
                         return;
                 }
 
-                (void) device_setup_unit(m, dev, node, /* main= */ false, NULL); /* 'dev' may be NULL. */
+                (void) device_setup_unit(m, dev, node, NULL); /* 'dev' may be NULL. */
         }
 
         /* Update the device unit's state, should it exist */
